0e180d6414f9e757c8f324d76d89a862e56b752a
Noah Mason <noahmason23@gmail.com>
2022-05-10 14:31:50 -0400

    Begins restructuring homepage.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import HomepageTable from '@site/src/components/HomepageTable';
+import styles from './index.module.css';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
@@ -14,19 +14,17 @@ function HomepageHeader() {
       <div className="container">
         <h1 className={styles.heroTitle}>{siteConfig.tagline}</h1>
         <div className={styles.heroDescWrapper}>
-          <p className={styles.heroDescription}>
-            Lorem ipsum dolor sit amet, praesent pericula imperdiet duo te. Duo
-            te aeterno reprimique ullamcorper. Eos commodo philosophia ex, et
-            per iudico quando, ut his commune necessitatibus.
-          </p>
-          <object
-            type="image/svg+xml"
-            width={470}
-            height={345}
-            data={'/img/hero-bg-image.svg'}
-          >
-            svg-animation
-          </object>
+          <div className={styles.heroDescription}>
+            <hr className={styles.heroDivider} />
+            <p>
+              Integrate secure provenance signals into your site, app, or
+              service using open-source tools developed by the Content
+              Authenticity Initiative. Join the ecosystem of transparency of
+              provenance and attribution of digital content to counter the rise
+              of misinformation.
+            </p>
+          </div>
+          <img className={styles.heroDescImage} src="/img/hero.svg" />
         </div>
       </div>
     </header>
@@ -35,7 +33,6 @@ function HomepageHeader() {
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
-  // This is where the landing page component can go
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
