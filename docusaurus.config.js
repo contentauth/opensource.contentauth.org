0ab124e95471bc45028f960a77abf457d1cad7f6
Noah Mason <noahmason23@gmail.com>
2022-05-10 14:20:10 -0400

    Updates tagine.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'CAI Open Source',
-  tagline: 'Open source tools for tracing media provenance',
+  tagline: 'Open-source tools for content authenticity and provenance',
   url: 'https://contentauth.netlify.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
