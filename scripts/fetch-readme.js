cef854cc5b68cbed2e7e55989949374e7ca03d3b
Dave Kozma <dkozma@adobe.com>
2022-06-04 22:23:29 -0400

    Update link parsing

--- scripts/fetch-readme.js
+++ scripts/fetch-readme.js
@@ -1,23 +1,45 @@
 const fetch = require('node-fetch');
 const { resolve } = require('path');
 const { writeFile } = require('fs/promises');
+const url = require('url');
+
+const GITHUB_HOST = 'https://github.com';
+const RAW_GITHUB_HOST = 'https://raw.githubusercontent.com';
+
+const mdLinkRegex =
+  /\[([\w\s\d\-]+)\]\(((?:\/|https?:\/\/|\.)[\w\d\-./?=#]+)\)/g;
 
 const readmes = [
   {
     dest: resolve(__dirname, '../docs/rust-sdk/readme.md'),
-    src: 'https://raw.githubusercontent.com/contentauth/c2pa-rs/main/README.md',
+    repo: 'contentauth/c2pa-rs',
+    path: 'README.md',
   },
   {
     dest: resolve(__dirname, '../docs/c2patool/readme.md'),
-    src: 'https://raw.githubusercontent.com/contentauth/c2pa-rs/main/c2patool/src/README.md',
+    repo: 'contentauth/c2pa-rs',
+    path: 'c2patool/src/README.md',
   },
 ];
 
+function resolveMarkdownLinks(linkBase, content) {
+  return content.replaceAll(mdLinkRegex, (match, label, href) => {
+    let revisedUrl = href;
+    if (!/^https?:\/\//.test(href)) {
+      revisedUrl = url.resolve(linkBase, href);
+    }
+    return `[${label}](${revisedUrl})`;
+  });
+}
+
 async function download() {
-  for await (const { src, dest } of readmes) {
+  for await (const { repo, path, dest } of readmes) {
+    const src = `${RAW_GITHUB_HOST}/${repo}/main/${path}`;
+    const linkBase = `${GITHUB_HOST}/${repo}/blob/main/${path}`;
     const res = await fetch(src);
     const markdown = await res.text();
-    await writeFile(dest, markdown, { encoding: 'utf-8', flag: 'w+' });
+    const resolvedMarkdown = resolveMarkdownLinks(linkBase, markdown);
+    await writeFile(dest, resolvedMarkdown, { encoding: 'utf-8', flag: 'w+' });
     console.log('Saved %s to %s', src, dest);
   }
 }
