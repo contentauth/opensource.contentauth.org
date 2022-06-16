70dfc2ef9184866eb862c3d3347a2ff311ed051c
Dave Kozma <dkozma@adobe.com>
2022-06-15 21:26:26 -0400

    Update c2pa-wc to 0.6.0

--- src/components/WebComponents/index.js
+++ src/components/WebComponents/index.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react';
 import 'c2pa-wc/dist/components/Icon';
 import 'c2pa-wc/dist/components/Indicator';
-import 'c2pa-wc/dist/components/Popover';
 import 'c2pa-wc/dist/components/panels/ManifestSummary';
 import 'c2pa-wc/dist/components/panels/PanelSection';
-import './App.css';
+import 'c2pa-wc/dist/components/Popover';
+import React, { useEffect, useRef, useState } from 'react';
+import './styles.css';
 
 export function WebComponents({ imageUrl, provenance, viewMoreUrl }) {
   const [manifest, setManifest] = useState(null);
@@ -35,7 +35,11 @@ export function WebComponents({ imageUrl, provenance, viewMoreUrl }) {
         <img src={imageUrl} />
         {manifest ? (
           <div>
-            <cai-popover interactive class="theme-spectrum">
+            <cai-popover
+              interactive
+              placement="left-start"
+              class="theme-spectrum"
+            >
               <cai-indicator slot="trigger"></cai-indicator>
               <cai-manifest-summary
                 ref={summaryRef}
