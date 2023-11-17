4049f9121ab640c70173d0d63721ff62092fccc8
Massy Mahamli <mmahamli@adobe.com>
2023-11-17 16:09:35 -0500

    html changes

--- docs/manifest/manifest-ref.js
+++ docs/manifest/manifest-ref.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './style.scss';
 
+const div = document.createElement('div');
 const markdown = require('!!raw-loader!./reference-cai.html')?.default; // MDX Context
+div.innerHTML = markdown;
+const data = div.innerHTML;
 
 const ManifestReference = () => {
-  return <div dangerouslySetInnerHTML={{ __html: markdown }} />;
+  return <div dangerouslySetInnerHTML={{ __html: data }} />;
 };
 
 export default ManifestReference;
