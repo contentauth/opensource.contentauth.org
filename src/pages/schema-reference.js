3e1b15560680d5813ef042c346d71337ffbbc8dd
Rand McKinney <crandmck@yahoo.com>
2025-11-06 18:27:22 -0800

    Change code for when component is called with no arg

--- src/pages/schema-reference.js
+++ src/pages/schema-reference.js
@@ -7,33 +7,12 @@ function useQuery() {
   return React.useMemo(() => new URLSearchParams(search), [search]);
 }
 
-const AVAILABLE_SCHEMAS = [
-  'Reader.schema.json',
-  'Builder.schema.json',
-  'ManifestDefinition.schema.json',
-  'Settings.schema.json',
-];
-
 export default function SchemaReferencePage() {
   const query = useQuery();
   const file = query.get('file');
 
   if (!file) {
-    return (
-      <div>
-        <h1>Schema Reference</h1>
-        <p>Select a schema to view its reference documentation:</p>
-        <ul>
-          {AVAILABLE_SCHEMAS.map((f) => (
-            <li key={f}>
-              <a href={`./schema-reference?file=${encodeURIComponent(f)}`}>
-                {f.replace('.schema.json', '')}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
+    return <div>ERROR: No schema file specified.</div>;
   }
 
   const schemaUrl = `/schemas/${file}`;
